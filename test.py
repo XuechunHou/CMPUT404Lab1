d7e3b80cfe62143de283b3ca031e3da92cb4147d
Xuechun Hou <xhou1@cs.ualberta.ca>
2020-01-08 17:59:45 -0700

    modified test.py

--- test.py
+++ test.py
@@ -3,4 +3,6 @@ import requests
 print (requests.__version__)
 google = requests.get("https://www.google.com")
 print(google)
+var = requests.get("https://raw.githubusercontent.com/XuechunHou/CMPUT404Lab1/master/test.py")
+print(var.content)
 
